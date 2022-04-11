0b9cc4bdeca95513184c81abe2a5105b79ce9611
Matheus Andrade <2002matheus@gmail.com>
2022-04-11 18:49:56 -0300

    Add data

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -19,6 +19,7 @@ const filmes = [
   {nome: 'Esposa de mentirinha', id: '02', img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQA8qAIRKHSShHbedNoIXsIk5W1x467VGiyvluBMQSO8iR9G1tDb6OSRf4-T4hnvibVZBU&usqp=CAU'},
   {nome: 'Vingadores Ultimato', id: '03', img: 'https://kanto.legiaodosherois.com.br/w760-h398-gnw-cfill-q95/wp-content/uploads/2019/05/legiao_aOjGzon9_hclWBK5g41Y6qMHASxZtbXimL2NEeIQD8.png.jpeg'},
   {nome: 'Tokyo Revengers', id: '04', img: 'https://pbs.twimg.com/media/E5ib1mmXIAA0cw4.jpg:large'},
+  {nome: 'Cada um tem a gêmea que parece', id: '05', img: 'http://4.bp.blogspot.com/-EAOluQNVeyo/TzQaV4zF0mI/AAAAAAAAAbQ/1-VaqYpruIE/w1200-h630-p-k-no-nu/cada-um-tem-a-irma-que-merece-poster-14ou2011.jpg'}
 ]
 
 const DashboardPage = ()=> {
