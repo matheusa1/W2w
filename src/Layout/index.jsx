80d006319c0cac374134554da08ed74e98e6b18e
Matheus Andrade <2002matheus@gmail.com>
2022-04-22 15:09:33 -0300

    Ajuste inicial

--- src/Layout/index.jsx
+++ src/Layout/index.jsx
@@ -1,64 +1,76 @@
-import { Layout, Menu } from 'antd';
-import { UploadOutlined, UserOutlined, VideoCameraOutlined, SearchOutlined } from '@ant-design/icons';
-import { NavLink } from 'react-router-dom';
-import * as S from './styles'
-import { useState } from 'react';
+import { Layout, Menu, Avatar } from "antd";
+import {
+  SearchOutlined,
+  HomeOutlined,
+  AppstoreOutlined,
+  SettingOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+import { NavLink } from "react-router-dom";
+import * as S from "./styles";
+import { useState } from "react";
+
 const { Header, Content, Footer, Sider } = Layout;
 
-const Applayout = ({children}) => {
-  const [showMenu,setShowMenu] = useState(true);
+const Applayout = ({ children }) => {
+  const [showMenu, setShowMenu] = useState(true);
 
-  const onCollapse = collapsed => {
-    setShowMenu(s=> !s);
+  const onCollapse = (collapsed) => {
+    setShowMenu((s) => !s);
   };
-  
-  return(
-  
 
-  <Layout style={{minHeight:'100vh'}}>
-    <Sider
-      collapsible 
-      collapsed={showMenu} 
-      // breakpoint="lg"
-      // collapsedWidth="0"
-      onBreakpoint={broken => {
-        console.log(broken);
-      }}
-      onCollapse={onCollapse}
+  return (
+    <Layout style={{ minHeight: "100vh" }}>
+      <Sider
+        collapsible
+        collapsed={showMenu}
+        breakpoint="lg"
+        collapsedWidth="50"
+        onBreakpoint={(broken) => {
+          console.log(broken);
+        }}
+        onCollapse={onCollapse}
       >
-      <div className="logo" />
-      <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
-        <Menu.Item key="1" icon={<UserOutlined />}>
-          <NavLink to="/">home</NavLink>
-        </Menu.Item>
-        <Menu.Item key="2" icon={<VideoCameraOutlined />}>
-        <NavLink to="/login">Login</NavLink>
-        </Menu.Item>
-        <Menu.Item key="3" icon={<UploadOutlined />}>
-        <NavLink to="/register">cadastro</NavLink>
-        </Menu.Item>
-        <Menu.Item key="4" icon={<UserOutlined />}>
-        <NavLink to="/filme/01">filme 01</NavLink>
-        </Menu.Item>
-        <Menu.Item key="5" icon={<UserOutlined />}>
-        <NavLink to="/filme/02">filme 02</NavLink>
-        </Menu.Item>
-        <Menu.Item key="6" icon={<SearchOutlined />}>
-        <NavLink to="/search">busca</NavLink>
-        </Menu.Item>
-      </Menu>
-    </Sider>
-    <Layout>
-      <Header className="site-layout-sub-header-background" style={{ padding: 0 }} />
-      <Content style={{ margin: '24px 16px 0' }}>
-        <S.LayoutFixHeight>
-          {children}
-          </S.LayoutFixHeight>
-      </Content>
-      {/* <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer> */}
+        <div className="logo" />
+        <Menu style={{paddingTop: "25px", textAlign: "center"}} theme="dark" mode="vertical" defaultSelectedKeys={["2"]}>
+          <Menu.Item key="1">
+            <Avatar
+              style={{
+                backgroundColor: "#5242cb",
+                marginRight: "10px",
+                marginLeft: "-10px",
+              }}
+              icon={<UserOutlined />}
+            />
+            <NavLink to="/profile">Perfil</NavLink>
+          </Menu.Item>
+          <S.Separator />
+          <Menu.Item key="2" icon={<HomeOutlined />}>
+            <NavLink to="/">Home</NavLink>
+          </Menu.Item>
+          <Menu.Item key="3" icon={<AppstoreOutlined />}>
+            <NavLink to="/category">Categoria</NavLink>
+          </Menu.Item>
+          <Menu.Item key="4" icon={<SearchOutlined />}>
+            <NavLink to="/search">Busca</NavLink>
+          </Menu.Item>
+          <Menu.Item key="5" icon={<SettingOutlined />}>
+            <NavLink to="/settings">Configurações</NavLink>
+          </Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout>
+        <Header
+          className="site-layout-sub-header-background"
+          style={{ padding: 0 }}
+        />
+        <Content style={{ margin: "24px 16px 0" }}>
+          <S.LayoutFixHeight>{children}</S.LayoutFixHeight>
+        </Content>
+        {/* <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer> */}
+      </Layout>
     </Layout>
-  </Layout>
-)}
+  );
+};
 
 export default Applayout;
-  
--- src/Layout/styles.js
+++ src/Layout/styles.js
@@ -1,6 +1,14 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const LayoutFixHeight = styled.div`
-  /* height: 50vh; */
-  /* background:white; */
-`
+  /* height: 50vh;
+  background:white; */
+`;
+export const Separator = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  height: 1px;
+  background: #272176;
+  margin: 0.5rem 0;
+`;
