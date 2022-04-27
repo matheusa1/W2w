16140ebf29b3c05c15c9319150acca486ae8f729
sSalvi <jaosalvi@protonmail.com>
2022-04-26 23:06:09 -0300

    Back semi-integrado

--- src/components/Trailer/index.jsx
+++ src/components/Trailer/index.jsx
@@ -1,10 +1,8 @@
-import { Image } from 'antd';
-import { PlayCircleFilled } from '@ant-design/icons';
 
 const Trailer = props => {
 
     return (
-        <iframe width="800" height="450" src="https://www.youtube.com/embed/KnamcFv_N9Q" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe width="800" height="450" src="https://www.youtube.com/embed/xEQP4VVuyrY" title="YouTube video player" frameborder="0" allowfullscreen></iframe>
     );
   }
   
--- src/pages/FilmesDetails/index.jsx
+++ src/pages/FilmesDetails/index.jsx
@@ -1,30 +1,40 @@
 import { Typography, Space, Affix, Divider, Button } from "antd";
 import  Axios  from "axios";
 import { useParams } from "react-router";
+import React, { useEffect, useState } from "react";
 import ClassifcEt from "../../components/ClassEt";
 import MediaBox from "../../components/MediaBox";
 import PosterImg from "../../components/MoviePic";
 import ReviewBox from "../../components/ReviewBox";
 
+
+
 const FilmeDetailsPage = props => {
   const { Text, Title } = Typography;
   
+  const [medias, setMedias] = useState();
+  
   const params = useParams();
   const { id } = params;
+
   const requestMedia = async () => {
     const axios = Axios;
-    const response = await axios.get(`http://localhost:1337/api/medias/2`)
+    const response = await axios.get(`http://localhost:1337/api/medias/${id}`)
+    setMedias(response.data.data);
     console.log(response.data);
   }
+  useEffect(() => {
+    requestMedia();
+  }, []);
   
-  
-  let titulo = "Magnolia";
-  let desc = "Um dia em San Fernando Valley, na Califórnia, nos arredores da rua Magnólia, as vidas de nove personagens são interligadas através de um programa de televisão onde um grupo de três crianças desafia três adultos, cujas histórias se cruzam por coincidências do destino.";
+  let titulo = medias?.attributes.title;
+  let desc = medias?.attributes.description;
   let posterLink = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/uq2u8HgtLFJkjNq2kHb2jvipIPT.jpg"
-  let trailerLink = "https://www.youtube.com/watch?v=KnamcFv_N9Q"
-  let rate = 9.2;
+  let trailerLink = medias?.attributes.link;
+  let rate = 9.2; //Tem que fazer os bagulho lá tá ligado? Soma das rating/quantidade de rating <--
   let rateIMDB = 8.0;
   let srcPics = ["https://www.themoviedb.org/t/p/original/4qpYhfkhWR1lFFtxjmmG9SrPDUs.jpg", "https://www.themoviedb.org/t/p/original/mND3PTdl7B6YKSLjuMTsCGXafcB.jpg", "https://www.themoviedb.org/t/p/original/fdMZRDhjQDwkNjpKrFN87D9NA8i.jpg"];
+  let releaseDate = medias?.attributes.releaseDate;
 
   return (
     <Space align="start">
@@ -36,7 +46,7 @@ const FilmeDetailsPage = props => {
                 </Space>
               </Affix>
               <Space align="Start" direction="vertical" size="small" style={{ display: 'flex' }}>
-                <Title level={1}>{titulo}</Title>
+                <Title italic level={2}>{titulo} - {releaseDate}</Title>
                 <Text italic style={{fontSize: 20}}>{desc}</Text>
                 <Divider style={{ borderWidth: 3 }}/> 
                 <Space align="start" style={{ display: 'flex' }}>
