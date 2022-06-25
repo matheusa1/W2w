359f30f8eb2fede025c6d7f8adbbc2c8364f330a
Matheus Andrade <2002matheus@gmail.com>
2022-06-24 21:11:17 -0300

    Map pronto

--- src/components/UserList/index.jsx
+++ src/components/UserList/index.jsx
@@ -1,44 +1,75 @@
 import { DeleteOutlined } from "@ant-design/icons";
-import { Row, Col, Button, Popconfirm, message } from "antd";
-import axios from "axios";
+import { Row, Col, Button, Popconfirm, message, Empty } from "antd";
+import Axios from "axios";
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
-import './index.css'
+import "./index.css";
 
-const UserList = props => {
+const UserList = (props) => {
+  const Lista = props.Filmes;
 
-  const [filmesStrapi,setFilmesStrapi] = useState([])
+  const [listaFilmesStrapi, setListaFilmesStrapi] = useState([]);
 
   const confirm = (e) => {
-      console.log(e)
-      message.success("Removido com sucesso")
-    }
+    console.log(e);
+    message.success("Removido com sucesso");
+  };
+
+  console.log(Lista);
+
+  const axios = Axios;
+
+  if (Lista) {
+    Lista.forEach(async (dado) => {
+      const response = await axios.get(
+        `http://localhost:1337/api/medias/${dado.strapiId}`
+      );
+      setListaFilmesStrapi((state) => [...state, response.data]);
+    });
+  }
 
   return (
     <>
-      {filmes.map(filme =>{
-        const response = await axios.get(`strapiapi/filme/${filme.id}`)
-        setFilmesStrapi(state => [...state,response.data.filme])
-        return(
+      {Lista ? (
+        Lista.lenght > 1 ? (listaFilmesStrapi.map(async (dado) => {
+          const posterLink =
+            dado?.attributes?.poster?.data[0]?.attributes?.name;
+          const title = dado?.attributes?.title;
+          return (
             <Row gutter={16}>
               <Col>
-              <NavLink>
+                <NavLink to="/filme/15">
                   <div className="image">
-                  <img class="image__img" style={{width: 150, height: 220}} alt="" src={props.Poster}/>
+                    <img
+                      class="image__img"
+                      style={{ width: 150, height: 220 }}
+                      alt={`poster ${title}`}
+                      src={posterLink}
+                    />
                     <div className="image__overlay">
-                      <div className="image__title">{props.Title}</div>
+                      <div className="image__title">{title}</div>
                     </div>
                   </div>
-              </NavLink>
-                <Popconfirm title="Deseja removê-lo da lista?" okText="Sim" cancelText="Nao" onConfirm={confirm}>
-                  <Button type="text" danger><DeleteOutlined /></Button>
+                </NavLink>
+                <Popconfirm
+                  title="Deseja removê-lo da lista?"
+                  okText="Sim"
+                  cancelText="Nao"
+                  onConfirm={confirm}
+                >
+                  <Button type="text" danger>
+                    <DeleteOutlined />
+                  </Button>
                 </Popconfirm>
-                </Col>
-              </Row>
+              </Col>
+            </Row>
           );
-        })}
+        })) : (<Empty description={false} />)
+      ) : (
+        <Empty description={false} />
+      )}
     </>
-  )
-}
+  );
+};
 
 export default UserList;
--- src/pages/Perfil/index.jsx
+++ src/pages/Perfil/index.jsx
@@ -4,58 +4,79 @@ import { useEffect, useState } from "react";
 import UserList from "../../components/UserList";
 import TextBox from "../../components/TextBox";
 import TotalWatched from "../../components/TotalWatched";
-import { useAuth } from "../../hooks/auth"
+import { useAuth } from "../../hooks/auth";
 
 const ProfilePage = () => {
-
-  const { token } = useAuth()
+  const { token } = useAuth();
   const [nick, setNick] = useState();
   const [loc, setLocation] = useState();
   const [desc, setDescription] = useState();
+  const [films, setFilms] = useState();
 
   const requestUser = async () => {
-    axios.defaults.headers.authorization = `Bearer ${token}`
-    const { data } = await axios.get("http://localhost:3333/users/me")
-    setNick(data.username)
-    setLocation(data.from)
-    setDescription(data.description)
-    console.log(data)
-  }
+    axios.defaults.headers.authorization = `Bearer ${token}`;
+    const { data } = await axios.get("http://localhost:3333/users/me");
+    setNick(data.username);
+    setLocation(data.from);
+    setDescription(data.description);
+    setFilms(data.myList);
+    console.log(data);
+  };
   useEffect(() => {
     requestUser();
   }, []);
- 
+
   let Watched = 10;
-  let Pic = 'https://super.abril.com.br/wp-content/uploads/2018/05/filhotes-de-cachorro-alcanc3a7am-o-c3a1pice-de-fofura-com-8-semanas1.png'
-  let Poster = "https://img.elo7.com.br/product/zoom/2C15759/big-poster-anime-black-clover-lo001-tamanho-90x60-cm-presente-nerd.jpg"
-  let Title = "Black Clover"
+  let Pic =
+    "https://super.abril.com.br/wp-content/uploads/2018/05/filhotes-de-cachorro-alcanc3a7am-o-c3a1pice-de-fofura-com-8-semanas1.png";
+  let Poster =
+    "https://img.elo7.com.br/product/zoom/2C15759/big-poster-anime-black-clover-lo001-tamanho-90x60-cm-presente-nerd.jpg";
+  let Title = "Black Clover";
 
   return (
-  <>
-    <Row justify="center">    
-        <Col xxl={{pull: 0, span: 6}} xl={{pull: 0, span: 6}} lg={{pull: 0, span: 6}} md={{pull: 0, span: 8}} sm={{pull: 0, span: 12}} xs={{ pull: 2, span: 16 }} >
-          <TextBox Name={nick} Bio={desc} Pic={Pic} From={loc}/>
+    <>
+      <Row justify="center">
+        <Col
+          xxl={{ pull: 0, span: 6 }}
+          xl={{ pull: 0, span: 6 }}
+          lg={{ pull: 0, span: 6 }}
+          md={{ pull: 0, span: 8 }}
+          sm={{ pull: 0, span: 12 }}
+          xs={{ pull: 2, span: 16 }}
+        >
+          <TextBox Name={nick} Bio={desc} Pic={Pic} From={loc} />
         </Col>
         <Row align="bottom">
-          <Col xxl={{push: 24}} xl={{push: 24}} lg={{push: 24}} md={{push: 24}} sm={{push: 24}} >
-            <TotalWatched Watched={Watched}/>
+          <Col
+            xxl={{ push: 24 }}
+            xl={{ push: 24 }}
+            lg={{ push: 24 }}
+            md={{ push: 24 }}
+            sm={{ push: 24 }}
+          >
+            {films ? (
+              <TotalWatched Watched={films.length} />
+            ) : (
+              <TotalWatched Watched={0} />
+            )}
           </Col>
         </Row>
-      <Divider/>
-      <div>
-        <Row justify="center">
-          <Col >
-            <h2>Minha Lista</h2>
-          </Col>
-        </Row>
-          <br/>
+        <Divider />
+        <div>
+          <Row justify="center">
+            <Col>
+              <h2>Minha Lista</h2>
+            </Col>
+          </Row>
+          <br />
           <Col>
-            <UserList Poster={Poster} Title={Title}/>
+            {/* <UserList Filmes={films} Poster={Poster} Title={Title}/> */}
+            <UserList Filmes={films} />
           </Col>
-      </div> 
-    </Row>
-  </>
-  )
+        </div>
+      </Row>
+    </>
+  );
 };
 
 export default ProfilePage;
